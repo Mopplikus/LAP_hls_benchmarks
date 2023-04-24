5733d2f357f15a97c2115d50802cf66871674f05
06Elija <elija.dirren@gmail.com>
2023-04-24 12:00:14 +0200

    Added missing tcl scripts

---
+++ Dynamatic/if_loop_2/vivado_scripts/synthesize_4.tcl
@@ -0,0 +1,27 @@
+read_vhdl -vhdl2008 ../sim/VHDL_SRC/arithmetic_units.vhd
+read_vhdl -vhdl2008 ../sim/VHDL_SRC/delay_buffer.vhd
+read_vhdl -vhdl2008 ../sim/VHDL_SRC/elastic_components.vhd
+read_vhdl -vhdl2008 ../sim/VHDL_SRC/MemCont.vhd
+read_vhdl -vhdl2008 ../sim/VHDL_SRC/multipliers.vhd
+read_vhdl -vhdl2008 ../sim/VHDL_SRC/mul_wrapper.vhd
+
+#read_verilog  ../sim/VHDL_SRC/LSQ_s.v
+#read_vhdl -vhdl2008 ../../../components/array_RAM_fadd_32bkb.vhd
+
+#source ../../../components/array_RAM_ap_fadd_8_full_dsp_32_ip.tcl
+
+read_vhdl -vhdl2008 ../sim/VHDL_SRC/if_loop_2_optimized.vhd
+
+#comment line below out if no clock constraints
+read_xdc period_4.xdc
+
+synth_design -top fir -part xc7k160tfbg484-1 -no_iobuf -mode out_of_context
+
+report_utilization > utilization_post_syn.rpt
+report_timing > timing_post_syn.rpt
+opt_design
+place_design
+route_design
+report_utilization > utilization_post_pr.rpt
+report_timing > timing_post_pr.rpt
+
