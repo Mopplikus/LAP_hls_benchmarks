ba5253cd2870027179e6ee32647a164aba1bccaa
06Elija <elija.dirren@gmail.com>
2023-05-10 17:03:44 +0200

    Added the scripts for the netlist

---
+++ utils/scripts_andrea/run_all_characterization.sh
@@ -0,0 +1,18 @@
+
+conn_type=("d" "r" "v" "m")
+conn_type=("d")
+
+bit_widths=("1"  "2"  "4"  "8"  "16"  "32"  "64")
+bit_widths=("1")
+
+for conn in ${conn_type[*]}; do
+
+	for bit in ${bit_widths[*]}; do
+
+		python run_characterization.py filelist.lst $conn $bit 
+
+	done
+
+done 
+
+
---
+++ utils/scripts_andrea/run_characterization.py
@@ -0,0 +1,287 @@
+#####################################################################################################
+# code to run characterization in vivado
+#
+# the first argument is the filelist containing the list of components to characterize
+#
+# the second argument is the type of connection
+# there are 4 types:
+#			- d(ata)
+#			- v(alid)
+#			- r(eady)
+#			- m(ixed)
+#
+# the third argument is the bitwidth to be considered
+# 
+# Example usage:
+#	python run_characterization.py filelist.lst d 3
+#
+#
+#####################################################################################################
+
+import sys, subprocess, re, os
+from multiprocessing import Process
+
+MAX_THREADS = 15
+
+def print_error(error_msg):
+	print("**ERRROR**")
+	print(error_msg)
+	exit()
+
+
+def create_subfile(cmp, file_in , file_pre, bit_width): #function to create file of component
+
+	file = open(file_pre, "r")
+	pre = file.read()
+	file.close()
+
+	component_desc = ""
+
+	start = False
+	for line in file_in:
+		if not(start) and re.match("entity "+ cmp  +" is", line)!=None:
+			start = True
+		if start:
+			if re.match("[Ll]ibrary(\\s+)*[Ii][Ee][Ee][Ee](\\s+)*;", line)!=None:
+				break
+			else:
+				line_d = line
+				if cmp != "TEHB" and cmp != "OEHB": #with 2 inputs the delay increases way more
+					line_d = re.sub("INPUTS(\\s)*:(\\s)*[iI]nteger","INPUTS: integer:=2", line_d)
+					line_d = re.sub("OUTPUTS(\\s)*:(\\s)*[iI]nteger","OUTPUTS: integer:=2", line_d)
+				else:
+					line_d = re.sub("INPUTS(\\s)*:(\\s)*[iI]nteger","INPUTS: integer:=1", line_d)
+					line_d = re.sub("OUTPUTS(\\s)*:(\\s)*[iI]nteger","OUTPUTS: integer:=1", line_d)
+
+				line_d = re.sub("INPUT_COUNT(\\s)*:(\\s)*[iI]nteger","INPUT_COUNT: integer:=2", line_d)
+				line_d = re.sub("OUTPUT_COUNT(\\s)*:(\\s)*[iI]nteger","OUTPUT_COUNT: integer:=2", line_d)
+				line_d = re.sub("[^_]SIZE(\\s)*:(\\s)*[Ii]nteger","SIZE: integer:=1", line_d)
+				line_d = re.sub("DATA_SIZE_IN(\\s)*:(\\s)*[Ii]nteger","DATA_SIZE_IN: integer:=" + bit_width, line_d)
+				line_d = re.sub("DATA_SIZE_OUT(\\s)*:(\\s)*[Ii]nteger","DATA_SIZE_OUT: integer:=" + bit_width, line_d)
+				line_d = re.sub("INPUT_SIZE(\\s)*:(\\s)*[Ii]nteger","INPUT_SIZE: integer:=" + bit_width, line_d)
+				line_d = re.sub("OUTPUT_SIZE(\\s)*:(\\s)*[Ii]nteger","OUTPUT_SIZE: integer:=" + bit_width, line_d)
+				line_d = re.sub("CONST_SIZE(\\s)*:(\\s)*[Ii]nteger","CONST_SIZE: integer:=1", line_d)
+				line_d = re.sub("COND_SIZE(\\s)*:(\\s)*[Ii]nteger","COND_SIZE: integer:=1", line_d)
+				line_d = re.sub("ADDRESS_SIZE(\\s)*:(\\s)*[Ii]nteger","ADDRESS_SIZE: integer:=" + str(pow(2,int(4))), line_d)
+				line_d = re.sub("FIFO_DEPTH(\\s)*:(\\s)*[Ii]nteger","FIFO_DEPTH: integer:=" + str(pow(2,int(4))), line_d)
+				line_d = re.sub("DATA_SIZE(\\s)*:(\\s)*[Ii]nteger","DATA_SIZE: integer:=" + bit_width, line_d)
+
+				#FOR MEMCONT
+				line_d = re.sub("DATA_SIZE(\\s)*:(\\s)*[Nn]atural","DATA_SIZE: natural:=" + bit_width, line_d)
+				line_d = re.sub("ADDRESS_SIZE(\\s)*:(\\s)*[Nn]atural","ADDRESS_SIZE: natural:=" + str(pow(2,int(4))), line_d)
+				line_d = re.sub("BB_COUNT(\\s)*:(\\s)*[Nn]atural","BB_COUNT: natural:=5", line_d)
+				line_d = re.sub("LOAD_COUNT(\\s)*:(\\s)*[Nn]atural","LOAD_COUNT: natural:=5", line_d)
+				line_d = re.sub("STORE_COUNT(\\s)*:(\\s)*[Nn]atural","STORE_COUNT: natural:=5", line_d)
+
+
+				component_desc += line_d
+
+	file_out = open(cmp+".vhd", "w")
+	file_out.write(pre+"\n\n")
+	file_out.write(component_desc)
+	file_out.close()
+
+def pins(inp, out):
+	input = None
+	output = None
+	if inp == "d":
+		input = "dataIn*"
+	elif inp == "v":
+		input = "pValid*"
+	elif inp == "r":
+		input = "nReady*"
+	elif inp == "c":
+		input = "condition*"
+	else:
+		print_error("Connection input pin not available")
+
+	if out == "d":
+		output = "dataOut*"
+	elif out == "v":
+		output = "valid*"
+	elif out == "r":
+		output = "ready*"
+	elif out == "c":
+		output = "condition"
+	else:
+		print_error("Connection outptu pin not available")
+	return input, output
+
+
+def get_pins_m(comp, conn_type, lib_mixed):
+	if conn_type == "m":
+		file = open(lib_mixed,"r")
+		found = ""
+		for line in file:
+			tmp = re.split("\\t+", line)
+			lib_comp = tmp[0]
+			if re.search(lib_comp, comp) != None:
+				found = tmp[1]
+		if found == "":
+			found = "vr" #if component not found it is assumed to be an operator
+		file.close()
+
+		conns = re.split("_", found)
+		input = []
+		output = []
+		for ele in conns:
+			if ele == "":
+				continue
+			conn = re.split("=", ele)[0]
+			inp, out = pins(conn[0], conn[1])
+			input.append(inp)
+			output.append(out)
+	else:
+		print_error("Connection path not available!")
+	return input, output
+
+
+def execute_vivado(tcl_file):
+	os.system("vivado-2019.1.1-bt vivado -mode batch -source " + tcl_file + "  >> rpt_"+tcl_file)
+
+
+args = sys.argv
+
+if len(args) != 4:
+	print_error("Number of args is not sufficient (3 needed)")
+
+file_name=args[1]
+
+file=open(file_name,"r")
+
+list_cmp = []
+
+for line in file:
+	list_cmp.append(line.replace("\n",""))
+
+file.close()
+
+conn_type = args[2]
+bit_width = args[3]
+
+lib_mixed = "filelist_mixed.lst"
+
+list_tcls = []
+list_mixed_conn = []
+
+for comp in list_cmp:
+	cmd = "grep -ir 'ENTITY " + comp  + " IS' vhdl/"
+	output = subprocess.check_output(cmd, shell=True)
+	file_name = output.split(":")[0]
+	file = open(file_name, "r")	
+	create_subfile(comp, file, "vhdl/pre.vhd", bit_width)
+	file.close()
+	syn_comp = "synthesis_" + comp + ".tcl"
+	os.system("cp synthesis.tcl " + syn_comp)
+	os.system("sed -i 's/TOP_DESIGN/"+ comp  +"/g' "+syn_comp)
+	
+	if conn_type != "m": #non-mixed case
+		input, output = pins(conn_type, conn_type)
+		os.system("sed -i 's/INPUT_PIN/"+ input  +"/g' "+syn_comp)
+		os.system("sed -i 's/OUTPUT_PIN/"+ output  +"/g' "+syn_comp)
+		os.system("sed -i 's/_CONN//g' "+syn_comp)
+		list_tcls.append(syn_comp)
+
+	else: #mixed case
+		input, output = get_pins_m(comp, conn_type, lib_mixed)
+		list_mixed_conn.append([]) 
+		for ind in range(len(input)):
+			in_ind = input[ind]
+			out_ind = output[ind]
+			syn_comp_ind = "synthesis_" + comp + "_"  + in_ind[0]  + out_ind[0]  +  ".tcl"
+			os.system("cp "+syn_comp + " " + syn_comp_ind)
+			os.system("sed -i 's/INPUT_PIN/"+ in_ind +"/g' "+syn_comp_ind)
+			os.system("sed -i 's/OUTPUT_PIN/"+ out_ind  +"/g' "+syn_comp_ind)
+			os.system("sed -i 's/_CONN/_" +  in_ind[0] + out_ind[0]  +   "/g' "+syn_comp_ind)
+			list_tcls.append(syn_comp_ind)
+			list_mixed_conn[-1].append(in_ind[0] + out_ind[0])
+
+cnt = 0
+num_threads = 0
+pool_threads = []
+
+while cnt < len(list_tcls):
+	while num_threads < MAX_THREADS and cnt < len(list_tcls):
+		tcl_file = list_tcls[cnt]
+
+		x = Process(target=execute_vivado, args=(tcl_file,)) 
+		pool_threads.append(x)
+		x.start()
+		num_threads += 1
+		cnt += 1
+	for x in pool_threads:
+		x.join()
+	num_threads = 0
+
+for comp_ind in range(len(list_cmp)):
+	comp = list_cmp[comp_ind]
+	comp_file = comp + ".vhd"
+	#os.system("rm " + comp_file)
+
+	synth_file = "synthesis_" + comp + ".tcl"
+	#os.system("rm " + synth_file)
+
+
+	if conn_type != "m":
+
+		rpt_file = "utilization_post_pr_" + comp + ".rpt"
+
+		if not(os.path.isfile(rpt_file)) :
+			continue
+
+		#os.system("rm " + rpt_file)
+
+		rpt_file = "timing_post_pr_" + comp + ".rpt"
+
+		if not(os.path.isfile(rpt_file)) :
+			continue
+
+		out_file = "timing_all_" + conn_type  +  "_" + bit_width  + ".rpt"
+		cmd = "grep 'Data Path Delay:' "+rpt_file
+
+		try:
+			delay = subprocess.check_output(cmd, shell=True).split()[3]
+		except(subprocess.CalledProcessError): #in case there is no path for this value
+			os.system("rm " + rpt_file)
+			continue
+
+		os.system("echo '"+comp+"\t\t" + delay  + "' >> " + out_file)
+
+
+	else:
+		conns = list_mixed_conn[comp_ind]
+		for ele in conns:
+			rpt_file = "utilization_post_pr_" + comp + "_" +ele+  ".rpt"
+
+			if not(os.path.isfile(rpt_file)) :
+				continue
+
+			#os.system("rm " + rpt_file)
+
+			rpt_file = "timing_post_pr_" + comp + "_" +ele+ ".rpt"
+
+			if not(os.path.isfile(rpt_file)) :
+				continue
+
+			out_file = "timing_all_" + conn_type  +  "_" + bit_width  + ".rpt"
+			cmd = "grep 'Data Path Delay:' "+rpt_file
+
+			try:
+				delay = subprocess.check_output(cmd, shell=True).split()[3]
+			except(subprocess.CalledProcessError): #in case there is no path for this value
+	#			os.system("rm " + rpt_file)
+				continue
+
+			os.system("echo '"+comp+"_" +ele+ "\t\t" + delay  + "' >> " + out_file)
+
+
+	#os.system("rm " + rpt_file)
+
+
+if conn_type == "m":
+	for tcl in list_tcls:
+		os.system("rm "+tcl)
+
+
+os.system("rm vivado*")
+#os.system("rm rpt_synthesis_*")
